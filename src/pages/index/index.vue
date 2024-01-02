3a3e9cb9780a38df2bc2f8a64798d0a4851a5d7f
zack <1126146235@qq.com>
2024-01-02 10:19:06 +0800

    feat(前台分类组件封装): ✨ 前台分类组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { getHomeBannerList } from '@/services/home';
 import { onLoad } from '@dcloudio/uni-app'
 import type { BannerItem } from '@/types/home';
@@ -20,10 +21,15 @@ onLoad(() => {
 
 <template>
   <CustomNavbar/>
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList"/>
+  <!-- 分类面板 -->
+  <CategoryPanel/>
   <!-- <view class="index">index</view> -->
 </template>
 
 <style lang="scss">
-//
+page{
+  background-color:#F7F7F7;
+}
 </style>
