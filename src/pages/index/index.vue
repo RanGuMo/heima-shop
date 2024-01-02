9f322c691d6d44aab11884e0dfe875eda85615b6
zack <1126146235@qq.com>
2024-01-02 09:53:15 +0800

    feat(首页 – 获取轮播图数据): ✨ 首页 – 获取轮播图数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
+import { getHomeBannerList } from '@/services/home';
+import { onLoad } from '@dcloudio/uni-app'
+
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerList()
+  console.log(res)
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
