64317bde120f84837716f2394c6d7357a8bc8f7a
zack <1126146235@qq.com>
2024-02-26 16:59:58 +0800

    feat(购物车模块): ✨ 购物车模块-底部计算信息

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -73,6 +73,35 @@ const onChangeSelectedAll = () => {
   // 后端数据更新
   putMemberCartSelectedAPI({ selected: _isSelectedAll })
 }
+
+// 6.计算选中单品列表
+const selectedCartList = computed(() => {
+  return cartList.value.filter((v) => v.selected)
+})
+
+// 6.1.计算选中总件数
+const selectedCartListCount = computed(() => {
+  return selectedCartList.value.reduce((sum, item) => sum + item.count, 0)
+})
+
+// 6.2.计算选中总金额
+const selectedCartListMoney = computed(() => {
+  return selectedCartList.value
+    .reduce((sum, item) => sum + item.count * item.nowPrice, 0)
+    .toFixed(2)
+})
+
+// 6.3.结算按钮
+const gotoPayment = () => {
+  if (selectedCartListCount.value === 0) {
+    return uni.showToast({
+      icon: 'none',
+      title: '请选择商品',
+    })
+  }
+  // 跳转到结算页
+  uni.showToast({ title: '等待完成' })
+}
 </script>
 
 <template>
@@ -146,9 +175,15 @@ const onChangeSelectedAll = () => {
       <view class="toolbar">
         <text @tap="onChangeSelectedAll" class="all" :class="{ checked: isSelectedAll }">全选</text>
         <text class="text">合计:</text>
-        <text class="amount">100</text>
+        <text class="amount">{{ selectedCartListMoney }}</text>
         <view class="button-grounp">
-          <view class="button payment-button" :class="{ disabled: true }"> 去结算(10) </view>
+          <view
+            @tap="gotoPayment"
+            class="button payment-button"
+            :class="{ disabled: selectedCartListCount === 0 }"
+          >
+            去结算({{ selectedCartListCount }})
+          </view>
         </view>
       </view>
     </template>
@@ -347,15 +382,18 @@ const onChangeSelectedAll = () => {
   align-items: center;
   flex-direction: column;
   height: 60vh;
+
   .image {
     width: 400rpx;
     height: 281rpx;
   }
+
   .text {
     color: #444;
     font-size: 26rpx;
     margin: 20rpx 0;
   }
+
   .button {
     width: 240rpx !important;
     height: 60rpx;
@@ -450,6 +488,7 @@ const onChangeSelectedAll = () => {
     }
   }
 }
+
 // 底部占位空盒子
 .toolbar-height {
   height: 100rpx;
