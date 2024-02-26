7b4f29e8f3f3abbd0df3b8eb33bd2e8b3dcb4168
zack <1126146235@qq.com>
2024-02-26 15:40:51 +0800

    feat( SKU模块): ✨ SKU模块-加入购物车

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -8,9 +8,11 @@ import PageSkeleton from './components/PageSkeleton.vue' //骨架屏
 import AddressPanel from './components/AddressPanel.vue' //地址弹出层
 import ServicePanel from './components/ServicePanel.vue' // 服务弹出层
 import type {
+  SkuPopupEvent,
   SkuPopupInstance,
   SkuPopupLocaldata,
 } from '@/components/vk-data-goods-sku-popup/vk-data-goods-sku-popup'
+import { postMemberCartAPI } from '@/services/cart'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -106,6 +108,13 @@ const skuPopupRef = ref<SkuPopupInstance>()
 const selectArrText = computed(() => {
   return skuPopupRef.value?.selectArr?.join(' ').trim() || '请选择商品规格'
 })
+
+// 10.加入购物车事件
+const onAddCart = async (ev: SkuPopupEvent) => {
+  await postMemberCartAPI({ skuId: ev._id, count: ev.buy_num })
+  uni.showToast({ title: '添加成功' })
+  isShowSku.value = false
+}
 </script>
 
 <template>
@@ -122,6 +131,7 @@ const selectArrText = computed(() => {
       borderColor: '#27BA9B',
       backgroundColor: '#E9F8F5',
     }"
+    @add-cart="onAddCart"
   />
   <!-- 弹窗测试 -->
   <!-- <button @tap="isShowSku = true">打开 SKU 弹窗</button> -->
